58f1728ecb32a193888943390643f3314fae9e0c
riiy zhou <riiyzhou@gmail.com>
2024-04-04 19:12:57 +0800

    delete vertico && add ivy

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -17,12 +17,12 @@
   (setq
     doom-themes-enable-bold t ; if nil, bold is universally disabled
     doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+  (load-theme 'doom-dracula t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; for treemacs users
-  (setq doom-themes-treemacs-theme "doom-one") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-dracula") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -38,6 +38,27 @@
     ("C-x C-f" . 'counsel-find-file) ; 使用 counsel 做文件打开操作，给予更多提示
     ("M-y" . 'counsel-yank-pop) ; 使用 counsel 做历史剪贴板粘贴，可以展示历史
     :map minibuffer-local-map ("C-r" . counsel-minibuffer-history)))
+
+;; ivy
+(use-package
+  ivy
+  :ensure t ; 确认安装，如果没有安装过 ivy 就自动安装
+  :init ; 在加载插件前执行命令
+  (ivy-mode 1) ; 启动 ivy-mode
+  (counsel-mode 1)
+  :custom ; 自定义一些变量，相当于赋值语句 (setq xxx yyy)
+  (ivy-use-virtual-buffers t) ; 一些官网提供的固定配置
+  (search-default-mode #'char-fold-to-regexp) (ivy-count-format "(%d/%d) ")
+  :bind ; 以下为绑定快捷键
+  (("C-s" . 'swiper-isearch) ; 绑定快捷键 C-s 为 swiper-search，替换原本的搜索功能
+    ("C-x b" . 'ivy-switch-buffer) ; 使用 ivy 做 buffer 切换，给予更多提示
+    ("C-c v" . 'ivy-push-view) ; 记录当前 buffer 的信息
+    ("C-c s" . 'ivy-switch-view) ; 切换到记录过的 buffer 位置
+    ("C-c V" . 'ivy-pop-view) ; 移除 buffer 记录
+    ("C-x C-SPC" . 'counsel-mark-ring) ; 使用 counsel 记录 mark 的位置
+    ("<f1> f" . 'counsel-describe-function)
+    ("<f1> v" . 'counsel-describe-variable)
+    ("<f1> i" . 'counsel-info-lookup-symbol)))
 (use-package which-key :ensure t :init (which-key-mode))
 
 ;; 输入法pyim-wbdict
@@ -68,134 +89,6 @@
     ("l" undo-tree-load-history)
     ("u" undo-tree-visualize "visualize" :color blue)
     ("q" nil "quit" :color blue)))
-;; Enable vertico
-(use-package
-  vertico
-  :ensure t
-  :init (vertico-mode)
-
-  ;; Different scroll margin
-  (setq vertico-scroll-margin 0)
-
-  ;; Show more candidates
-  (setq vertico-count 20)
-
-  ;; Grow and shrink the Vertico minibuffer
-  (setq vertico-resize t)
-
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  (setq vertico-cycle t))
-
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist :ensure t :init (savehist-mode))
-;; A few more useful configurations...
-(use-package
-  emacs
-  :ensure t
-  :init
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete)
-
-  ;; Emacs 30 and newer: Disable Ispell completion function. As an alternative,
-  ;; try `cape-dict'.
-  (setq text-mode-ispell-word-completion nil)
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun
-    crm-indicator (args)
-    (cons
-      (format
-        "[CRM%s] %s"
-        (replace-regexp-in-string "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" "" crm-separator)
-        (car args))
-      (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-  ;; Support opening new minibuffers from inside existing minibuffers.
-  (setq enable-recursive-minibuffers t)
-
-  ;; Emacs 28 and newer: Hide commands in M-x which do not work in the current
-  ;; mode.  Vertico commands are hidden in normal buffers. This setting is
-  ;; useful beyond Vertico.
-  (setq read-extended-command-predicate #'command-completion-default-include-p))
-;; Optionally use the `orderless' completion style.
-(use-package
-  orderless
-  :ensure t
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq
-    completion-styles
-    '(orderless basic)
-    completion-category-defaults
-    nil
-    completion-category-overrides
-    '((file (styles partial-completion)))))
-;; Use Dabbrev with Corfu!
-(use-package
-  dabbrev
-  :ensure t
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion) ("C-M-/" . dabbrev-expand))
-  :config (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
-  ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
-  (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
-  (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode))
-;; Consult users will also want the embark-consult package.
-(use-package embark-consult :ensure t :defer t)
-(use-package
-  embark
-  :ensure t
-  :bind
-  (:map
-    minibuffer-local-map
-    ("M-o" . embark-act)
-    ("C-c C-c" . embark-export)
-    ("C-c C-o" . embark-collect)))
-
-(use-package
-  consult
-  :ensure t
-  :bind
-  (([remap imenu] . consult-imenu)
-    ([remap goto-line] . consult-goto-line)
-    ([remap bookmark-jump] . consult-bookmark)
-    ([remap recentf-open-files] . consult-recent-file)
-    ([remap repeat-complex-command] . consult-complex-command)
-    ([remap jump-to-register] . consult-register-load)
-    ([remap point-to-register] . consult-register-store))
-  :config
-  (with-no-warnings
-    (consult-customize
-      consult-ripgrep
-      consult-git-grep
-      consult-grep
-      consult-bookmark
-      consult-recent-file
-      consult-buffer
-      :preview-key nil))
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay 0.5 register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
-  (advice-add #'register-preview :override #'consult-register-window)
-  :custom
-  (consult-fontify-preserve nil)
-  (consult-async-min-input 2)
-  (consult-async-refresh-delay 0.15)
-  (consult-async-input-throttle 0.2)
-  (consult-async-input-debounce 0.1))
 
 (provide 'init-utils)
 ;; Local Variables:
