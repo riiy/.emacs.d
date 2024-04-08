2a22142460432c6cf16bd04771de2a6a370f2a42
riiy zhou <riiyzhou@gmail.com>
2024-04-08 14:29:08 +0800

    delete treemacs-persp

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -253,13 +253,6 @@
 (use-package treemacs-magit :ensure t :after (treemacs magit) :ensure t)
 
 (use-package
-  treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
-  :ensure t
-  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
-  :ensure t
-  :config (treemacs-set-scope-type 'Perspectives))
-
-(use-package
   treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
   :ensure t
   :after (treemacs)
