ea26aac6276eec3e6710e7d6dda18f5c53b6711d
riiy zhou <riiyzhou@gmail.com>
2024-04-11 10:06:02 +0800

    update org capture

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -121,6 +121,129 @@
       ("PROJ" . +org-todo-project)
       ("NO" . +org-todo-cancel)
       ("KILL" . +org-todo-cancel)))
+  (defvar
+    +org-capture-todo-file "todo.org"
+    "Default target for todo entries.
+
+Is relative to `org-directory', unless it is absolute. Is used in Doom's default
+`org-capture-templates'.")
+
+  (defvar
+    +org-capture-changelog-file "changelog.org"
+    "Default target for changelog entries.
+
+Is relative to `org-directory' unless it is absolute. Is used in Doom's default
+`org-capture-templates'.")
+
+  (defvar
+    +org-capture-notes-file "notes.org"
+    "Default target for storing notes.
+
+Used as a fall back file for org-capture.el, for templates that do not specify a
+target file.
+
+Is relative to `org-directory', unless it is absolute. Is used in Doom's default
+`org-capture-templates'.")
+
+  (defvar
+    +org-capture-journal-file "journal.org"
+    "Default target for storing timestamped journal entries.
+
+Is relative to `org-directory', unless it is absolute. Is used in Doom's default
+`org-capture-templates'.")
+
+  (defvar
+    +org-capture-projects-file
+    "projects.org"
+    "Default, centralized target for org-capture templates.")
+
+  (defvar +org-habit-graph-padding 2 "The padding added to the end of the consistency graph")
+
+  (defvar
+    +org-habit-min-width
+    30
+    "Hides the consistency graph if the `org-habit-graph-column' is less than this value")
+
+  (defvar
+    +org-habit-graph-window-ratio
+    0.3
+    "The ratio of the consistency graphs relative to the window width")
+
+  (defvar
+    +org-startup-with-animated-gifs
+    nil
+    "If non-nil, and the cursor is over a gif inline-image preview, animate it!")
+  (setq
+    org-default-notes-file
+    (expand-file-name +org-capture-notes-file org-directory)
+    +org-capture-journal-file
+    (expand-file-name +org-capture-journal-file org-directory)
+    org-capture-templates
+    '
+    (
+      ("t"
+        "Personal todo"
+        entry
+        (file+headline +org-capture-todo-file "Inbox")
+        "* [ ] %?\n%i\n%a"
+        :prepend t)
+      ("n"
+        "Personal notes"
+        entry
+        (file+headline +org-capture-notes-file "Inbox")
+        "* %u %?\n%i\n%a"
+        :prepend t)
+      ("j"
+        "Journal"
+        entry
+        (file+olp+datetree +org-capture-journal-file)
+        "* %U %?\n%i\n%a"
+        :prepend t)
+
+      ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
+      ;; {todo,notes,changelog}.org file is found in a parent directory.
+      ;; Uses the basename from `+org-capture-todo-file',
+      ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
+      ("p" "Templates for projects")
+      ("pt" "Project-local todo" entry ; {project-root}/todo.org
+        (file+headline +org-capture-project-todo-file "Inbox") "* TODO %?\n%i\n%a"
+        :prepend t)
+      ("pn" "Project-local notes" entry ; {project-root}/notes.org
+        (file+headline +org-capture-project-notes-file "Inbox") "* %U %?\n%i\n%a"
+        :prepend t)
+      ("pc" "Project-local changelog" entry ; {project-root}/changelog.org
+        (file+headline +org-capture-project-changelog-file "Unreleased") "* %U %?\n%i\n%a"
+        :prepend t)
+
+      ;; Will use {org-directory}/{+org-capture-projects-file} and store
+      ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
+      ;; support `:parents' to specify what headings to put them under, e.g.
+      ;; :parents ("Projects")
+      ("o" "Centralized templates for projects")
+      ("ot"
+        "Project todo"
+        entry
+        (function +org-capture-central-project-todo-file)
+        "* TODO %?\n %i\n %a"
+        :heading "Tasks"
+        :prepend nil)
+      ("on"
+        "Project notes"
+        entry
+        (function +org-capture-central-project-notes-file)
+        "* %U %?\n %i\n %a"
+        :heading "Notes"
+        :prepend t)
+      ("oc"
+        "Project changelog"
+        entry
+        (function +org-capture-central-project-changelog-file)
+        "* %U %?\n %i\n %a"
+        :heading "Changelog"
+        :prepend t)))
+
+  (add-hook 'org-after-refile-insert-hook #'save-buffer)
+
   :bind (("C-c a" . 'org-agenda) :map org-mode-map ("C-c C-q" . counsel-org-tag)))
 
 (use-package
