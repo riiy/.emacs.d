07beebb1d36fab7932d49dfeab9ac47a3f64bf35
riiy zhou <riiyzhou@gmail.com>
2024-04-11 11:30:34 +0800

    commentary lsp

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -41,27 +41,6 @@
   :hook (prog-mode . flycheck-mode))
 
 (use-package
-  lsp-mode
-  :ensure t
-  :init (setq lsp-keymap-prefix "C-c b" lsp-file-watch-threshold 500)
-  :hook
-  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
-  (setq lsp-headerline-breadcrumb-enable t)
-  :bind
-  ("C-c b s" . lsp-ivy-workspace-symbol)) ;; 可快速搜索工作区内的符号（类名、函数名、变量名等）
-(use-package
-  lsp-ui
-  :ensure t
-  :config
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (setq lsp-ui-doc-position 'top))
-(use-package lsp-ivy :ensure t)
-
-(use-package
   projectile
   :ensure t
   :init (projectile-mode +1)
@@ -71,52 +50,63 @@
 (use-package diff-hl :ensure t :config (global-diff-hl-mode) (diff-hl-margin-mode))
 (use-package rg :ensure t)
 
-(use-package
-  c++-mode
-  :functions ; suppress warnings
-  c-toggle-hungry-state
-  :hook (c-mode . lsp-deferred) (c++-mode . lsp-deferred) (c++-mode . c-toggle-hungry-state))
-(use-package
-  python
-  :defer t
-  :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python3" . python-mode))
+;; (use-package
+;;   lsp-mode
+;;   :ensure t
+;;   :init (setq lsp-keymap-prefix "C-c b" lsp-file-watch-threshold 500)
+;;   :hook
+;;   (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
+;;   :commands (lsp lsp-deferred)
+;;   :config
+;;   (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
+;;   (setq lsp-headerline-breadcrumb-enable t)
+;;   :bind
+;;   ("C-c b s" . lsp-ivy-workspace-symbol)) ;; 可快速搜索工作区内的符号（类名、函数名、变量名等）
+;; (use-package
+;;   lsp-ui
+;;   :ensure t
+;;   :config
+;;   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;;   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+;;   (setq lsp-ui-doc-position 'top))
+;; (use-package lsp-ivy :ensure t)
 
-(use-package
-  pyvenv
-  :ensure t
-  :config
-  ;; (setenv "WORKON_HOME" (expand-file-name "~/miniconda3/envs"))
-  ;; (setq python-shell-interpreter "python3")  ; （可选）更改解释器名字
-  (pyvenv-mode t)
-  ;; （可选）如果希望启动后激活 miniconda 的 base 环境，就使用如下的 hook
-  ;; :hook
-  ;; (python-mode . (lambda () (pyvenv-workon "..")))
-  )
-(use-package
-  lsp-pyright
-  :ensure t
-  :config
-  :hook (python-mode . (lambda () (require 'lsp-pyright) (lsp-deferred))))
-(use-package
-  ccls
-  :ensure t
-  :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda () (require 'ccls) (lsp))))
+;; (use-package
+;;   c++-mode
+;;   :functions ; suppress warnings
+;;   c-toggle-hungry-state
+;;   :hook (c-mode . lsp-deferred) (c++-mode . lsp-deferred) (c++-mode . c-toggle-hungry-state))
+;; (use-package
+;;   python
+;;   :defer t
+;;   :mode ("\\.py\\'" . python-mode)
+;;   :interpreter ("python3" . python-mode))
+
+;; (use-package
+;;   pyvenv
+;;   :ensure t
+;;   :config
+;;   ;; (setenv "WORKON_HOME" (expand-file-name "~/miniconda3/envs"))
+;;   ;; (setq python-shell-interpreter "python3")  ; （可选）更改解释器名字
+;;   (pyvenv-mode t)
+;;   ;; （可选）如果希望启动后激活 miniconda 的 base 环境，就使用如下的 hook
+;;   ;; :hook
+;;   ;; (python-mode . (lambda () (pyvenv-workon "..")))
+;;   )
+;; (use-package
+;;   lsp-pyright
+;;   :ensure t
+;;   :config
+;;   :hook (python-mode . (lambda () (require 'lsp-pyright) (lsp-deferred))))
+;; (use-package
+;;   ccls
+;;   :ensure t
+;;   :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda () (require 'ccls) (lsp))))
+
 ;; commentary
 (use-package evil-nerd-commenter :ensure t :config (evilnc-default-hotkeys))
 
 (use-package restclient :ensure t :mode (("\\.http\\'" . restclient-mode)))
-
-(use-package
-  beancount
-  :load-path "site-lisp/package/beancount-mode" ;; git@github.com:beancount/beancount-mode.git
-  :init
-  (add-hook 'beancount-mode-hook (lambda () (setq-local electric-indent-chars nil)))
-  (add-hook 'beancount-mode-hook #'flymake-bean-check-enable)
-  :mode (("\\.beancount\\'" . beancount-mode))
-  :config
-  (define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
-  (define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading))
 
 (provide 'init-dev)
 
