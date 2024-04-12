f9c4971d0a4cae12d3b3577d0f6fd389065d2c2f
riiy <riiyzhou@gmail.com>
2024-04-12 20:56:17 +0800

    delete lsp-bridge

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -8,22 +8,8 @@
 (use-package nerd-icons :ensure t :custom (nerd-icons-font-family "Symbols Nerd Font Mono"))
 
 (use-package doom-modeline :ensure t :init (doom-modeline-mode 1))
-
-(use-package
-  doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq
-    doom-themes-enable-bold t ; if nil, bold is universally disabled
-    doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dracula t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
+;; themes
+(load-theme 'dracula t)
 ;; 用不同颜色标记多级括号
 (use-package rainbow-delimiters :ensure t :hook (prog-mode . rainbow-delimiters-mode))
 (use-package nerd-icons-dired :ensure t :hook (dired-mode . nerd-icons-dired-mode))
