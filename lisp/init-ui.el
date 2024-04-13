514b9a51ba1ac6551db3355529bf4c6618876eb6
riiy zhou <riiyzhou@gmail.com>
2024-04-13 23:03:03 +0800

    update ui, delete doom-themes

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -9,20 +9,9 @@
 
 (use-package doom-modeline :ensure t :init (doom-modeline-mode 1))
 
-(use-package
-  doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq
-    doom-themes-enable-bold t ; if nil, bold is universally disabled
-    doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dracula t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+(load-theme 'manoj-dark)
+;; (load-theme 'modus-vivendi)
+;; (load-theme 'wheatgrass)
 
 ;; 用不同颜色标记多级括号
 (use-package rainbow-delimiters :ensure t :hook (prog-mode . rainbow-delimiters-mode))
