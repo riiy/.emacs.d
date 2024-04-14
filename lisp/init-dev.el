2a878771a35d2c21ef178f34be06c896f7ceed19
riiy zhou <riiyzhou@gmail.com>
2024-04-14 08:21:43 +0800

    add eglot

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -2,6 +2,37 @@
 ;;; Commentary:
 
 ;;; Code:
+(use-package
+  eglot
+  :ensure t
+  :hook (((python-mode) . eglot-ensure) ((cider-mode eglot-managed-mode) . eglot-disable-in-cider))
+  :preface
+  (defun
+    eglot-disable-in-cider ()
+    (when
+      (eglot-managed-p)
+      (if
+        (bound-and-true-p cider-mode)
+        (progn
+          (remove-hook 'completion-at-point-functions 'eglot-completion-at-point t)
+          (remove-hook 'xref-backend-functions 'eglot-xref-backend t))
+        (add-hook 'completion-at-point-functions 'eglot-completion-at-point nil t)
+        (add-hook 'xref-backend-functions 'eglot-xref-backend nil t))))
+  :custom
+  (eglot-autoshutdown t)
+  (eglot-events-buffer-size 0)
+  (eglot-extend-to-xref nil)
+  (eglot-ignored-server-capabilities
+    '
+    (:hoverProvider
+      :documentHighlightProvider
+      :documentFormattingProvider
+      :documentRangeFormattingProvider
+      :documentOnTypeFormattingProvider
+      :colorProvider
+      :foldingRangeProvider))
+  (eglot-stay-out-of '(yasnippet))
+  :config (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio"))))
 
 (provide 'init-dev)
 
