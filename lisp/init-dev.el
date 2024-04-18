b80dc08d1e39e27875f9256abfe53184c6f34ea6
riiy <riiyzhou@gmail.com>
2024-04-18 10:07:25 +0800

    update eglot

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -5,19 +5,9 @@
 (use-package
   eglot
   :ensure t
-  :hook (((python-mode) . eglot-ensure) ((cider-mode eglot-managed-mode) . eglot-disable-in-cider))
-  :preface
-  (defun
-    eglot-disable-in-cider ()
-    (when
-      (eglot-managed-p)
-      (if
-        (bound-and-true-p cider-mode)
-        (progn
-          (remove-hook 'completion-at-point-functions 'eglot-completion-at-point t)
-          (remove-hook 'xref-backend-functions 'eglot-xref-backend t))
-        (add-hook 'completion-at-point-functions 'eglot-completion-at-point nil t)
-        (add-hook 'xref-backend-functions 'eglot-xref-backend nil t))))
+  :defer t
+  :bind (:map eglot-mode-map ("C-c C-r" . eglot-rename))
+  :hook (((python-mode) . eglot-ensure))
   :custom
   (eglot-autoshutdown t)
   (eglot-events-buffer-size 0)
@@ -35,7 +25,7 @@
   :config
   ;; Python specific
   (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
-  (setq lsp-pyright-use-library-code-for-types t) 
+  (setq lsp-pyright-use-library-code-for-types t)
   (setq lsp-pyright-stub-path (concat (getenv "HOME") "/wokspc/python-type-stubs"))
   (setq-default eglot-workspace-configuration '((:pyright . ((useLibraryCodeForTypes . t))))))
 
