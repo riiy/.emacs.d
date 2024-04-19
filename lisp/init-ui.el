d3032c0b5588b94c5219c97280b982949ff6fa3e
riiy <riiyzhou@gmail.com>
2024-04-19 16:02:55 +0800

    update load path

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -6,7 +6,7 @@
 ;;; Code:
 
 ;; themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory)) ; 设定主题加载路径
 (load-theme 'dracula t)
 ;; 用不同颜色标记多级括号
 (use-package rainbow-delimiters :ensure t :hook (prog-mode . rainbow-delimiters-mode))
