be53b9ca6235950ed886007b90c1e893a6f8e695
riiy zhou <riiyzhou@gmail.com>
2024-05-28 15:07:47 +0800

    update org-agenda bind key

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -244,7 +244,7 @@ Is relative to `org-directory', unless it is absolute. Is used in Doom's default
 
   (add-hook 'org-after-refile-insert-hook #'save-buffer)
 
-  :bind (("C-c a" . 'org-agenda) ("C-x c" . 'org-capture)))
+  :bind (("C-x a a" . 'org-agenda) ("C-x c" . 'org-capture)))
 
 (use-package
   evil-org
